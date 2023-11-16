7e63474883c7003a44e215c72deafbc3c76ac117
iveambassador <150927281+iveambassador@users.noreply.github.com>
2023-11-16 03:02:28 +0000

    set the template

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Iveliz Ayala",
+  title: "Software Engineer",
+  email: "iveliz.ayalalopez@gmail.com",
+  gitHub: "iveambassador",
+  instagram: "starriixx",
+  linkedIn: "iveliz alexandra ayala lopez",
+  twitter: "Ayalaiveliz",
 };
 
 const primaryColor = "#4E567E";
